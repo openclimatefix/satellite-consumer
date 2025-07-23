1c78c340e26adb1cf35fbdc2f7b17e10c00b3f19
devsjc <47188100+devsjc@users.noreply.github.com>
2025-07-23 14:59:23 +0100

    feat(icechunk): Rebase on failure (#40) #minor
    * feat(icechunk): Rebase on failure
    
    * fix(run): Linting
    
    * fix(run): fix
    
    * feat(pyproject): Upgrade icechunk
    
    * fix(run): Only write encoding on first dataset write
    
    * feat(repo): Add extract-latest command
    
    * docs(README): Add sentry env vars
    
    * chore(repo): Typing

--- src/satellite_consumer/run.py
+++ src/satellite_consumer/run.py
@@ -2,17 +2,19 @@
 
 Consolidates the old cli_downloader, backfill_hrv and backfill_nonhrv scripts.
 """
-import datetime as dt
-import os
-import itertools
 
+import datetime as dt
+import itertools
+import os
 import warnings
 from collections.abc import Generator, Iterable
 from importlib.metadata import PackageNotFoundError, version
-from typing import TYPE_CHECKING, TypeVar
+from typing import TypeVar
 
 import eumdac.product
+import icechunk
 import numpy as np
+import sentry_sdk
 from icechunk.xarray import to_icechunk
 from joblib import Parallel, delayed
 from loguru import logger as log
@@ -20,7 +22,7 @@ from loguru import logger as log
 from satellite_consumer import storage
 from satellite_consumer.config import (
     ConsumeCommandOptions,
-    MergeCommandOptions,
+    ExtractLatestCommandOptions,
     SatelliteConsumerConfig,
 )
 from satellite_consumer.download_eumetsat import (
@@ -31,28 +33,12 @@ from satellite_consumer.exceptions import ValidationError
 from satellite_consumer.process import process_raw
 from satellite_consumer.validate import validate
 
-if TYPE_CHECKING:
-    import icechunk
-
 T = TypeVar("T")
 
 try:
     __version__ = version("satellite-consumer")
 except PackageNotFoundError:
     __version__ = "v?"
-
-
-
-import sentry_sdk
-
-sentry_sdk.init(
-    dsn=os.getenv("SENTRY_DSN"),
-    environment=os.getenv("ENVIRONMENT", "local"),
-    traces_sample_rate=1,
-)
-sentry_sdk.set_tag("app_name", "satellite_consumer")
-sentry_sdk.set_tag("app_version", __version__)
-
 
 
 def _consume_to_store(command_opts: ConsumeCommandOptions) -> None:
@@ -79,7 +65,7 @@ def _consume_to_store(command_opts: ConsumeCommandOptions) -> None:
                     return
                 yield batch
 
-        for product_batch in _batcher(product_iter, command_opts.num_workers):
+        for batch_num, product_batch in enumerate(_batcher(product_iter, command_opts.num_workers)):
             log.debug("Processing batch of products", num_products=len(product_batch))
 
             # Download the files for each product in the batch in parallel
@@ -117,7 +103,7 @@ def _consume_to_store(command_opts: ConsumeCommandOptions) -> None:
                     dst=command_opts.zarr_path,
                     time=str(np.datetime_as_string(da.coords["time"].values[0], unit="m")),
                 )
-                if i == 0 and len(existing_times) == 0:
+                if len(existing_times) == 0 and batch_num == 0 and i == 0:
                     session: icechunk.Session = repo.writable_session(branch="main")
                     # TODO: Remove warnings catch when Zarr makes up its mind about codecs
                     with warnings.catch_warnings(action="ignore"):
@@ -126,7 +112,10 @@ def _consume_to_store(command_opts: ConsumeCommandOptions) -> None:
                             session=session,
                             mode="w-",
                             encoding={
-                                "time": {"units": "nanoseconds since 1970-01-01"},
+                                "time": {
+                                    "units": "nanoseconds since 1970-01-01",
+                                    "calendar": "proleptic_gregorian",
+                                },
                                 "data": {"dtype": "f4"},
                             },
                         )
@@ -144,6 +133,8 @@ def _consume_to_store(command_opts: ConsumeCommandOptions) -> None:
                         )
                     _ = session.commit(
                         message=f"add {len(da.coords['time']) * len(da.coords['variable'])} images",
+                        rebase_with=icechunk.ConflictDetector(),
+                        rebase_tries=5,
                     )
                 num_written += 1
                 processed_filepaths.extend(raw_filepaths)
@@ -217,36 +208,17 @@ def _consume_to_store(command_opts: ConsumeCommandOptions) -> None:
             validate(src=command_opts.zarr_path)
 
 
-def _merge_command(command_opts: MergeCommandOptions) -> None:
+def _extract_latest_command(command_opts: ExtractLatestCommandOptions) -> None:
     """Logic for the merge command."""
-    zarr_paths = command_opts.zarr_paths
-    log.info(
-        f"Merging {len(zarr_paths)} stores",
-        num=len(zarr_paths),
-        consume_missing=command_opts.consume_missing,
+    log.info(f"Extracting latest data from {command_opts.zarr_path}")
+    desired_image_num: int = (
+        command_opts.window_mins // command_opts.satellite_metadata.cadence_mins
     )
-    fs = storage.get_fs(path=zarr_paths[0])
 
-    for zarr_path in zarr_paths:
-        if not fs.exists(zarr_path):
-            if command_opts.consume_missing:
-                _consume_to_store(
-                    command_opts=ConsumeCommandOptions(
-                        time=dt.datetime.strptime(
-                            zarr_path.split("/")[-1].split("_")[0],
-                            "%Y%m%dT%H%M",
-                        ).replace(tzinfo=dt.UTC),
-                        satellite=command_opts.satellite,
-                        workdir=command_opts.workdir,
-                        validate=True,
-                        rescale=True,  # TODO: Make this an option
-                        resolution=command_opts.resolution,
-                    ),
-                )
-            else:
-                raise FileNotFoundError(f"Zarr store not found at {zarr_path}")
-
-    dst = storage.create_latest_zip(srcs=zarr_paths)
+    dst = storage.create_latest_zip(
+        src=command_opts.zarr_path,
+        time_slice=slice(-desired_image_num, None),
+    )
     log.info("Created latest.zip", dst=dst)
 
 
@@ -261,10 +233,19 @@ def run(config: SatelliteConsumerConfig) -> None:
         opts=config.command_options.__str__(),
     )
 
+    if os.getenv("SENTRY_DSN", "") != "":
+        sentry_sdk.init(
+            dsn=os.environ["SENTRY_DSN"],
+            environment=os.getenv("ENVIRONMENT", "local"),
+            traces_sample_rate=1,
+        )
+        sentry_sdk.set_tag("app_name", "satellite_consumer")
+        sentry_sdk.set_tag("app_version", __version__)
+
     if isinstance(config.command_options, ConsumeCommandOptions):
         _consume_to_store(command_opts=config.command_options)
-    elif isinstance(config.command_options, MergeCommandOptions):
-        _merge_command(command_opts=config.command_options)
+    elif isinstance(config.command_options, ExtractLatestCommandOptions):
+        _extract_latest_command(command_opts=config.command_options)
     else:
         pass
 
