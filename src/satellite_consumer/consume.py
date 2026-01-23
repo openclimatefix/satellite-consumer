313541e1c66d9619191366f02a28f92219ab9ee2
James Fulton <41546094+dfulu@users.noreply.github.com>
2026-01-23 10:14:13 +0000

    perf(consumer): move quality filter to consumer to avoid unnecessary serialization (#91)
    * Move product quality check to main process
    
    * Add warning on skip for quality

--- src/satellite_consumer/consume.py
+++ src/satellite_consumer/consume.py
@@ -216,6 +216,16 @@ async def consume_to_store(
         )
         return rounded_time not in existing_times
 
+    def _nominal_quality(product: eumdac.product.Product) -> bool:
+        if product.qualityStatus == "NOMINAL":
+            return True
+        else:
+            log.warning(f"skipping product {product}: qualityStatus is {product.qualityStatus}")
+            return False
+
+    def _product_filter(product: eumdac.product.Product) -> bool:
+        return _nominal_quality(product) and _not_stored(product)
+
     # Iterate through all products in search
     num_skips: int = 0
     total_num: int = 0
@@ -224,7 +234,7 @@ async def consume_to_store(
     t_last: float = 0
     get_iter_time_ema = EMA()
     async for item in _buffered_apply(
-        filter(_not_stored, product_iter),
+        filter(_product_filter, product_iter),
         bound_func,
         buffer_size=buffer_size,
         max_workers=max_workers,
