edc7ca514985a4526eb02c243060e3adda25fe93
James Fulton <41546094+dfulu@users.noreply.github.com>
2026-02-02 14:49:41 +0000

    feat(config): Add configurable timeout (#103)

--- src/satellite_consumer/cmd/main.py
+++ src/satellite_consumer/cmd/main.py
@@ -109,6 +109,7 @@ def main() -> None:
             accum_writes=conf.get_int("consumer.accum_writes"),
             executor=conf.get_string("consumer.executor"),
             jump_to_latest=conf.get_bool("consumer.jump_to_latest"),
+            request_timeout=conf.get_int("consumer.request_timeout"),
         ),
     )
 
